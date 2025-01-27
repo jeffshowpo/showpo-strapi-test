98ba8b9f05f61d31cc3bf4635844ecec0fee7c98
jeffshowpo <jeff@showpo.com>
2025-01-28 07:57:57 +0800

    fix homepage controller

--- src/api/home-page/controllers/home-page.ts
+++ src/api/home-page/controllers/home-page.ts
@@ -4,11 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController(
-  'api::home-page.home-page',
-  ({ strapi }) => ({
-    async find(ctx) {
-      const { data, meta } = await super.find(ctx);
-    },
-  })
-);
+export default factories.createCoreController('api::home-page.home-page');
