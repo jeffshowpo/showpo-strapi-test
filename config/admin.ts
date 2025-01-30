7b9015505babc42a1b3c09039eb7dcb00c3f536f
jeffshowpo <jeff@showpo.com>
2025-01-30 14:44:16 +0800

    add preview

--- config/admin.ts
+++ config/admin.ts
@@ -14,4 +14,36 @@ export default ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+  preview: {
+    enabled: true,
+    config: {
+      allowedOrigins: [env('CLIENT_URL')], // Usually the frontend application URL
+      async handler(uid, { documentId, locale, status }) {
+        const document = await strapi.documents(uid).findOne({ documentId });
+
+        console.log(arguments);
+
+        const pathname = getPreviewPathname(uid, { locale, document });
+
+        const previewUrl = `${env('CLIENT_URL')}${pathname}`;
+
+        // console.log(previewUrl);
+
+        return previewUrl;
+      },
+    },
+  },
 });
+
+// Function to generate preview pathname based on content type and document
+const getPreviewPathname = (uid, { locale, document }): string => {
+  // Handle different content types with their specific URL patterns
+  switch (uid) {
+    // Handle pages with predefined routes
+    case 'api::home-page.home-page':
+      return `/content/strapi-home-page?preview=true`;
+    default: {
+      return null;
+    }
+  }
+};
